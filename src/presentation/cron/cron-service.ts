5cda3e32f6aa27e6951f50f93c4573d33285240c
OscarLinux <oclemente.dscont@gmail.com>
2024-04-17 15:12:53 -0500

    Actualizando proyecto

--- src/presentation/cron/cron-service.ts
+++ src/presentation/cron/cron-service.ts
@@ -1,20 +1,21 @@
-import{ CronJob} from 'cron';
+import { CronJob } from 'cron';
 
 type CronTime = string | Date;
-
 type OnTick = () => void;
 
-export class CronService {
 
-    static createJob(cronTime:CronTime, onTick:OnTick):CronJob {
+export class CronService  {
 
-        const job = new CronJob(cronTime,onTick,);
-        job.start();
-        //job.stop();
 
-        return job;
-    }
+  static createJob( cronTime: CronTime, onTick: OnTick ): CronJob {
+
+    const job = new CronJob( cronTime,onTick );
+    
+    job.start();
+    
+    return job;
+
+  }
+
 
 }
-
-
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,35 +1,25 @@
-
-import { error } from "console";
-import { CheckService } from "../domain/use-cases/checks/check-service";
-import { CronService } from "./cron/cron-service";
+import { CheckService } from '../domain/use-cases/checks/check-service';
+import { CronService } from './cron/cron-service';
 
 
+export class Server {
 
-export class Server{
+  public static start() {
 
-    static start():void{
+    console.log( 'Server started...' );
 
-        console.log('server started F...');
+    
+    CronService.createJob(
+      '*/5 * * * * *',
+      () => {
+        const url = 'https://google.com';
+        new CheckService(
+          () => console.log( `${ url } is ok` ),
+          ( error ) => console.log( error ),
+        ).execute( url );
+        // new CheckService().execute( 'http://localhost:3000' );        
+      }
+    );
+  }
 
-        CronService.createJob(
-            '*/5 * * * * *',
-            ()=>{
-                const url = 'https://google.com';
-                new CheckService(
-                    ()=> console.log(`${url} is ok`),
-                    ()=> console.log(error)
-                ).execute(url);
-                //new CheckService().execute('http://localhost:3000');
-            }
-        );
-
-        // CronService.createJob(
-        //     '*/2 * * * * *',
-        //     ()=>{
-        //         const date= new Date();
-        //         console.log('2 seconds', date);
-        //     }
-        // );
-
-    }
 }
