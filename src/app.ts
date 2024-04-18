9a804dff76cc13089523bd5d2a0fb05c05c51c57
OscarLinux <oclemente.dscont@gmail.com>
2024-04-18 17:03:31 -0500

    fbdsf

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { envs } from './config/plugins/envs.plugin';
+import { LogModel, MongoDatabase } from './data/mongo';
 import { Server } from './presentation/server';
 
 
@@ -6,8 +8,32 @@ import { Server } from './presentation/server';
 })();
 
 
-function main(){
-  Server.start();
+async function main(){
+  
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  })
+
+  // Crear una  coleccion = tables, documento= registro
+  // const newLog = await LogModel.create({
+  //   message: 'Test mesage desde Mongo',    
+  //   origin: 'App.ts',
+  //   level: 'low'
+  // });
+
+  // await newLog.save();
+  // console.log(newLog);
+
+  // const logs = await LogModel.find();  
+  //console.log(logs[0]);
+  const fechaHoraActual = new Date();
+  const fechaHoraActualLocal = fechaHoraActual.toLocaleString();
+  console.log(fechaHoraActual);
+  console.log(fechaHoraActualLocal);
+
+
+  // Server.start();
 }
 
 
